a949393af13e178ef0cac58424abe15b229cadef
amr mahmoud <amrmahmoud1900@gmail.com>
2024-06-15 14:31:57 +0300

    add user create publisher

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import 'express-async-errors'
 import mongoose from 'mongoose';
 import {app} from "./app";
+import {natsWrapper} from "./nats-wrapper";
 
 const start = async () => {
     process.env.JWT_KEY = "amrmahmoud"
@@ -10,7 +11,26 @@ const start = async () => {
     // if (!process.env.MONGO_URL){
     //     throw  new Error('Mongo url doesnt exist');
     // }
+    if (!process.env.NATS_URL){
+        throw  new Error('NATS_URL doesnt exist');
+    }
+    if (!process.env.NATS_CLUSTER_ID){
+        throw  new Error('NATS_CLUSTER_ID doesnt exist');
+    }
+    if (!process.env.NATS_CLIENT_ID){
+        throw  new Error('NATS_CLIENT_ID doesnt exist');
+    }
     try{
+        await natsWrapper.connect(
+            process.env.NATS_CLUSTER_ID,
+            process.env.NATS_CLIENT_ID,
+            process.env.NATS_URL
+        );
+        natsWrapper.client.on('close',()=>{
+            console.log('nats connection closed!');
+        });
+        process.on('SIGTERM', ()=> natsWrapper.client.close());
+        process.on('SIGINT', ()=> natsWrapper.client.close());
         // await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
         await mongoose.connect('mongodb://localhost:27017/auth2' );
         console.log("DB connection");
