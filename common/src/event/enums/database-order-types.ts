066ba52955c4cc6fead5f720aaf3a2e11cba74a5
amr mahmoud <amrmahmoud1900@gmail.com>
2024-06-20 05:34:25 +0300

    update

--- common/src/event/enums/database-order-types.ts
+++ common/src/event/enums/database-order-types.ts
@@ -1,4 +1,4 @@
-export enum OatabaseOrderType {
+export enum DatabaseOrderType {
     DatabaseMongoDB = 'Database:MongoDB',
     DatabasePostgresSQl = 'Database:PostgresSQL',
     DatabaseMySQL = 'Database:MySQL',
