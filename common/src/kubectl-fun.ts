d65c48c02162d695c62cee982269b91a7fe5471c
amr mahmoud <amrmahmoud1900@gmail.com>
2024-06-24 09:07:18 +0300

    update kubectl fun

--- common/src/kubectl-fun.ts
+++ common/src/kubectl-fun.ts
@@ -2,32 +2,42 @@ import {k8sAppsApi, k8sCoreApi} from "./kubctl-connection";
 
 export class KubectlFun {
     private namespace: string  = 'default';
-    private podStatus: string | undefined;
-    private podPort: string | undefined
-    async getPodStatus(deploymentName: string){
-        const deploymentRes = await k8sAppsApi.readNamespacedDeployment(deploymentName, this.namespace);
-        const selectorLabels = deploymentRes.body.spec!.selector.matchLabels;
-        // @ts-ignore
-        const labelSelector = Object.entries(selectorLabels).map(([key, value]) => `${key}=${value}`).join(',');
-        const podsRes = await k8sCoreApi.listNamespacedPod(this.namespace
-            , undefined, undefined, undefined, undefined, labelSelector);
-        const podNames = podsRes.body.items.map(pod => pod.metadata!.name);
-        const podName = podNames[0];
-        const podRes = await k8sCoreApi.readNamespacedPod(podName!, this.namespace);
-        this.podStatus = podRes.body.status!.phase?.toString();
-        return this.podStatus;
+    async getPodStatus(deploymentName: string): Promise<string> {
+        try {
+            // Fetch deployment details
+            const deploymentRes = await k8sAppsApi.readNamespacedDeployment(deploymentName, this.namespace);
+            const selectorLabels = deploymentRes.body.spec!.selector.matchLabels;
+
+            // @ts-ignore
+            const labelSelector = Object.entries(selectorLabels)
+                .map(([key, value]) => `${key}=${value}`)
+                .join(',');
+
+            // List pods using the label selector
+            const podsRes = await k8sCoreApi.listNamespacedPod(this.namespace, undefined, undefined, undefined, undefined, labelSelector);
+            const podNames = podsRes.body.items.map(pod => pod.metadata!.name);
+            const podName = podNames[0];
+
+            // Fetch the status of the first pod
+            const podRes = await k8sCoreApi.readNamespacedPod(podName!, this.namespace);
+            const podStatus = podRes.body.status!.phase?.toString();
+
+            // Return the pod status
+            return podStatus || "Unknown"; // Default to "Unknown" if status is not found
+        } catch (error) {
+            console.error('Error getting pod status:', error);
+            throw new Error('Failed to get pod status');
+        }
     }
-    getstatus(deploymentName: string){
-        this.getPodStatus(deploymentName);
-        return this.podStatus;
-    }
-    async getPodPort(serviceName: string){
-        const svcRes = await k8sCoreApi.readNamespacedService(serviceName, this.namespace);
-        this.podPort = svcRes.body.spec!.ports!.find(port => port.nodePort)?.nodePort?.toString();
-    }
-    getPort(serviceName: string){
-        this.getPodPort(serviceName);
-        return this.podPort;
+    async getPodPort(serviceName: string): Promise<string | undefined> {
+        try {
+            const svcRes = await k8sCoreApi.readNamespacedService(serviceName, this.namespace);
+            const podPort = svcRes.body.spec!.ports!.find(port => port.nodePort)?.nodePort?.toString();
+            return podPort;
+        } catch (error) {
+            console.error('Error getting pod port:', error);
+            throw new Error('Failed to get pod port');
+        }
     }
 }
 
