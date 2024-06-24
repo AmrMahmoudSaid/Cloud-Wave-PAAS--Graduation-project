1a5fb2a8678672ee69bb38b94bc78b545d3347eb
amr mahmoud <amrmahmoud1900@gmail.com>
2024-06-24 08:39:34 +0300

    update kubectl fun

--- common/src/kubectl-fun.ts
+++ common/src/kubectl-fun.ts
@@ -2,6 +2,8 @@ import {k8sAppsApi, k8sCoreApi} from "./kubctl-connection";
 
 export class KubectlFun {
     private namespace: string  = 'default';
+    private podStatus: string | undefined;
+    private podPort: string | undefined
     async getPodStatus(deploymentName: string){
         const deploymentRes = await k8sAppsApi.readNamespacedDeployment(deploymentName, this.namespace);
         const selectorLabels = deploymentRes.body.spec!.selector.matchLabels;
@@ -12,12 +14,20 @@ export class KubectlFun {
         const podNames = podsRes.body.items.map(pod => pod.metadata!.name);
         const podName = podNames[0];
         const podRes = await k8sCoreApi.readNamespacedPod(podName!, this.namespace);
-        return podRes.body.status!.phase;
+        this.podStatus = podRes.body.status!.phase?.toString();
+        return this.podStatus;
+    }
+    getstatus(deploymentName: string){
+        this.getPodStatus(deploymentName);
+        return this.podStatus;
     }
     async getPodPort(serviceName: string){
         const svcRes = await k8sCoreApi.readNamespacedService(serviceName, this.namespace);
-        return svcRes.body.spec!.ports!.find(port => port.nodePort)?.nodePort?.toString();
-
+        this.podPort = svcRes.body.spec!.ports!.find(port => port.nodePort)?.nodePort?.toString();
+    }
+    getPort(serviceName: string){
+        this.getPodPort(serviceName);
+        return this.podPort;
     }
 }
 
