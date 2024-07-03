b8a3ce1708d751ad45f6f8bc463e0b42dac7b604
youssefashraf1 <100059932+youssefashraf1@users.noreply.github.com>
2024-07-03 09:38:47 +0300

    adding regex patterns and changing website icon and name

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,23 +8,29 @@ import PaymentForm from "./pages/Payment";
 import AddDatabase from "./pages/AddDatabase";
 import AddApplication from "./pages/AddApplication";
 import AppLogs from "./pages/AppLogs";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/loginForm" element={<LoginForm />} />
-        <Route path="/homepage" element={<HomePage />} />
-        <Route path="/payment" element={<Payment />} />
-        <Route path="/paymentform" element={<PaymentForm />} />
-        <Route path="/createdatabase" element={<AddDatabase />} />
-        <Route path="/createapplication" element={<AddApplication />} />
-        <Route path="/applogs" element={<AppLogs />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <Helmet>
+        <title>Cloud wave</title>
+      </Helmet>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/loginForm" element={<LoginForm />} />
+          <Route path="/homepage" element={<HomePage />} />
+          <Route path="/payment" element={<Payment />} />
+          <Route path="/paymentform" element={<PaymentForm />} />
+          <Route path="/createdatabase" element={<AddDatabase />} />
+          <Route path="/createapplication" element={<AddApplication />} />
+          <Route path="/applogs" element={<AppLogs />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import logo from "../assets/logo.png";
+
 function SignupForm() {
   const [name, setName] = useState("");
-  const [email, setEmail] = useState("omar@example.com");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const namepatern = /^[A-Za-z\d]{3,}$/;
+  const pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+  const passwordPattern = /^[A-Za-z\d]{8,}$/;
 
   const navigate = useNavigate();
 
@@ -15,28 +19,29 @@ function SignupForm() {
       localStorage.setItem("name", name);
       localStorage.setItem("email", email);
       localStorage.setItem("password", password);
-      console.log("ahmeddd");
-      if (localStorage.getItem("email") && localStorage.getItem("password")) {
+
+      if (email && password) {
         navigate("/payment");
       } else {
         navigate("/signup");
       }
     } catch (error) {
-      console.error("Login failed", error);
+      console.error("Signup failed", error);
     }
   };
+
   return (
     <div className="flex items-center justify-center min-h-screen bg-red-500">
       <form
         onSubmit={handleSubmit}
-        className="lg:w-[450px] lg:h-[650px] md:w-[320px] md:h-[500px] bg-white rounded-2xl text-black border border-black border-solid justify-center text-left items-center px-16 py-6 absolute lg:right-[10%] lg:top-[5%] md:right-[5%] md:top-[13%] "
+        className="lg:w-[450px] lg:h-[650px] md:w-[320px] md:h-[500px] bg-white rounded-2xl text-black border border-black border-solid justify-center text-left items-center px-16 py-6 absolute lg:right-[10%] lg:top-[5%] md:right-[5%] md:top-[13%]"
       >
         <div className="flex items-center gap-2 lg:mb-6 md:mb-2">
           <div className="lg:w-24 lg:h-auto md:w-[80px] md:h-auto">
             <img src={logo} alt="Logo" className="w-full h-full object-cover" />
           </div>
         </div>
-        <div className="md:text-base lg:text-2xl font-semibold lg:mb-4 ">
+        <div className="md:text-base lg:text-2xl font-semibold lg:mb-4">
           Welcome to Cloud Wave
         </div>
         <div className="md:text-xs lg:text-sm opacity-70 mb-4 leading-none">
@@ -49,8 +54,19 @@ function SignupForm() {
           value={name}
           onChange={(e) => setName(e.target.value)}
           placeholder="Enter your username"
-          className="w-full lg:px-4 lg:py-2 md:px-2 md:py-0 mb-4 md:mb-2 border border-gray-300 rounded"
+          className={`w-full lg:px-4 lg:py-2 md:px-2 md:py-0 mb-4 md:mb-2 border ${
+            !name || namepatern.test(name)
+              ? "border-gray-300"
+              : "border-red-500"
+          } rounded`}
         />
+        {!name || namepatern.test(name) ? null : (
+          <ul>
+            <li>
+              <p className="text-red-500">name at least 3 characters</p>
+            </li>
+          </ul>
+        )}
         <label className="block mb-2 md:text-sm">Email</label>
         <input
           type="text"
@@ -58,8 +74,18 @@ function SignupForm() {
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           placeholder="Enter your email"
-          className="w-full lg:px-4 lg:py-2 md:px-2 md:py-0 mb-4 md:mb-2 border border-gray-300 rounded"
+          className={`w-full lg:px-4 lg:py-2 md:px-2 md:py-0 mb-4 md:mb-2 border ${
+            !email || pattern.test(email) ? "border-gray-300" : "border-red-500"
+          } rounded`}
         />
+        {!email || pattern.test(email) ? null : (
+          <ul>
+            <li>
+              <p className="text-red-500">Enter a valid email</p>
+            </li>
+          </ul>
+        )}
+
         <label className="block mb-2 md:text-sm">Password</label>
         <input
           type="password"
@@ -67,13 +93,39 @@ function SignupForm() {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           placeholder="Enter your password"
-          className="w-full lg:px-4 lg:py-2 md:px-2 md:py-0 mb-4 md:mb-2 border border-gray-300 rounded"
+          className={`w-full lg:px-4 lg:py-2 md:px-2 md:py-0 mb-4 md:mb-2 border ${
+            !password || passwordPattern.test(password)
+              ? "border-gray-300"
+              : "border-red-500"
+          } rounded`}
         />
+        {!password || passwordPattern.test(password) ? null : (
+          <ul>
+            <li>
+              <p className="text-red-500">password at least 8 characters</p>
+            </li>
+          </ul>
+        )}
+
         <input
           type="submit"
           value="Signup"
-          className="w-full lg:px-4 lg:py-2 md:px-2 md:py-1 mb-4 bg-blue-500 text-white rounded cursor-pointer hover:bg-blue-700 ease-in-out	duration-500"
+          disabled={
+            !email ||
+            !password ||
+            !pattern.test(email) ||
+            !passwordPattern.test(password)
+          }
+          className={`w-full lg:px-4 lg:py-2 md:px-2 md:py-1 mb-4 ${
+            !email ||
+            !password ||
+            !pattern.test(email) ||
+            !passwordPattern.test(password)
+              ? "bg-blue-500 cursor-not-allowed"
+              : "bg-blue-500 hover:bg-blue-700"
+          } text-white rounded cursor-pointer ease-in-out duration-500`}
         />
+
         <div className="mb-4">
           New to our platform?
           <Link to="/signup" className="text-blue-500">
